aa3298c4dcffb3c29b75bce09fbc6ceec7052eb2
Jose Sarmiento <jose.dev.official@gmail.com>
2022-06-21 06:22:11 +0800

    replace __dirname with cwd

--- server/server.js
+++ server/server.js
@@ -44,9 +44,9 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 // serving static assets from react client
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(process.cwd(), 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(process.cwd(), 'client', 'build', 'index.html'));
   });
 }
 
